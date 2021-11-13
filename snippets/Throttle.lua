7ff22e7645fefbcee3703b3cd9a737ae3def4401
Anaminus <anaminus@protonmail.com>
2021-11-13 08:26:31 -0600

    Include variant that cancels the call.

--- snippets/Throttle.lua
+++ snippets/Throttle.lua
@@ -5,7 +5,7 @@ local function Throttle(rate, func)
 		return func
 	end
 	local running = false
-	return function(...)
+	local function throttledCallback(...)
 		if running then
 			return
 		end
@@ -15,4 +15,31 @@ local function Throttle(rate, func)
 			func(...)
 		end, ...)
 	end
+	return throttledCallback
+end
+
+-- Also returns a function that cancels the call.
+local function Throttle(rate, func)
+	if rate <= 0 then
+		return func
+	end
+	local running = false
+	local canceled = false
+	local function throttledCallback(...)
+		if running or canceled then
+			return
+		end
+		running = true
+		task.delay(rate, function(...)
+			if canceled then
+				return
+			end
+			running = false
+			func(...)
+		end, ...)
+	end
+	local function cancel()
+		canceled = true
+	end)
+	return throttledCallback, cancel
 end
