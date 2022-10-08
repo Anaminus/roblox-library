5ac5ca1692a7ac7c824bf63fc3080c30ca29fc33
Anaminus <anaminus@protonmail.com>
2022-10-08 06:46:16 -0500

    Update SignalFire.
    - Use "export" instead of "SignalFire" for export table.
    - Annotate with Luau types.
    - Restructure statements to satisfy Luau limitations.
    - Include argument name in error messages.
    - `all` and `any` each return a Destroyer.
    - `any` will continue to fire until destroyed.
    - Ensure disconnectors are called only once.
    - Add `limit` function that limits number of times a signal is fired.
    - Allow wrapped signal to receive threads as listeners.
    - Add Bindable, which matches BindableEvent API.

--- modules/SignalFire/README.md
+++ modules/SignalFire/README.md
@@ -8,9 +8,9 @@ Notable differences from Roblox's Signal pattern:
 - Everything is a function.
 - Listeners may be threads as well as functions.
 - Listeners are always deferred.
-- Listeners are unordered. Relying on this was always a bad idea.
-- After a signal is fired, every listener with a connection *at the time of
-  firing* will be invoked.
+- Listeners are unordered.
+- After a signal is fired, only every listener with a connection *at the time
+  of firing* will be invoked.
 
 [observer]: https://en.wikipedia.org/wiki/Observer_pattern
 
@@ -20,15 +20,20 @@ Notable differences from Roblox's Signal pattern:
 
 1. [SignalFire][SignalFire]
 	1. [SignalFire.new][SignalFire.new]
-	2. [SignalFire.all][SignalFire.all]
-	3. [SignalFire.any][SignalFire.any]
-	4. [SignalFire.wait][SignalFire.wait]
-	5. [SignalFire.wrap][SignalFire.wrap]
+	2. [SignalFire.bindable][SignalFire.bindable]
+	3. [SignalFire.all][SignalFire.all]
+	4. [SignalFire.any][SignalFire.any]
+	5. [SignalFire.limit][SignalFire.limit]
+	6. [SignalFire.wait][SignalFire.wait]
+	7. [SignalFire.wrap][SignalFire.wrap]
 2. [Connector][Connector]
 3. [Listener][Listener]
 4. [Disconnector][Disconnector]
-5. [Destroyer][Destroyer]
-6. [Fire][Fire]
+5. [Fire][Fire]
+6. [Destroyer][Destroyer]
+7. [Bindable][Bindable]
+8. [Signal][Signal]
+9. [Connection][Connection]
 
 </td></tr></tbody>
 </table>
@@ -42,26 +47,44 @@ function SignalFire.new(): (Connector, Fire, Destroyer)
 The **new** constructor returns a signal, represented by associated
 [Connector][Connector], [Fire][Fire] and [Destroyer][Destroyer] functions.
 
+## SignalFire.bindable
+[SignalFire.bindable]: #user-content-signalfirebindable
+
+The **bindable** constructor returns a new [Bindable][Bindable],
+implemented using the functions from [SignalFire.new][SignalFire.new].
+
 ## SignalFire.all
 [SignalFire.all]: #user-content-signalfireall
 ```
-function SignalFire.all(...: Connector): Connector
+function SignalFire.all(...: Connector): (Connector, Destroyer)
 ```
 
-The **all** constructor returns the [Connector][Connector] of a signal
-that fires after all of the signals associated with the given connectors have
-fired. The signal fires up to one time.
+The **all** function returns the [Connector][Connector] and
+[Destroyer][Destroyer] of a signal that fires after all of the signals
+associated with the given connectors have fired. The signal will fire up to
+one time.
 
 ## SignalFire.any
 [SignalFire.any]: #user-content-signalfireany
 ```
-function SignalFire.any(...: Connector): Connector
+function SignalFire.any(...: Connector): (Connector, Destroyer)
 ```
 
-The **any** constructor returns the [Connector][Connector] of a signal
-that fires after any of the signals associated with the given connectors have
-fired. The signal passes the arguments of the first signal that fired it. The
-signal fires up to one time.
+The **any** function returns the [Connector][Connector] and
+[Destroyer][Destroyer] of a signal that fires after any of the signals
+associated with the given connectors have fired. The signal passes the
+arguments of the first signal that fired it. The signal will fire until
+destroyed.
+
+## SignalFire.limit
+[SignalFire.limit]: #user-content-signalfirelimit
+```
+function SignalFire.limit(connect: Connector, limit: number?): Connector
+```
+
+The **limit** function wraps *connect*, returning a
+[Connector][Connector] that will cause its connected [Listeners][Listener] to
+be fired only up to *limit* times. The limit defaults to 1.
 
 ## SignalFire.wait
 [SignalFire.wait]: #user-content-signalfirewait
@@ -69,8 +92,8 @@ signal fires up to one time.
 function SignalFire.wait(connect: Connector): (() -> (...any))
 ```
 
-The **wait** constructor returns a function that, when called, yields
-the running thread. The thread is resumed after the signal associated with
+The **wait** function returns a function that, when called, yields the
+running thread. The thread is resumed after the signal associated with
 *connect* fires, returning the arguments passed through the signal.
 
 ## SignalFire.wrap
@@ -79,11 +102,10 @@ the running thread. The thread is resumed after the signal associated with
 function SignalFire.wrap(signal: RBXScriptSignal): Connector
 ```
 
-The **wrap** constructor returns a [Connector][Connector] that wraps
-*signal*. [Listeners][Listener] passed to the connector must be passable to
-[RBXScriptSignal.Connect][Connect].
-
-[Connect]: https://developer.roblox.com/en-us/api-reference/datatype/RBXScriptSignal#functions
+The **wrap** function returns a [Connector][Connector] that wraps
+*signal*. If the connector is passed a thread as a [Listener][Listener], it
+is connected via a function that calls task.defer with the thread and the
+received arguments.
 
 # Connector
 [Connector]: #user-content-connector
@@ -118,6 +140,20 @@ type Disconnector = () -> ()
 A **Disconnector** breaks the connection of a [Listener][Listener] to a
 signal when called. Does nothing if the connection is already broken.
 
+# Fire
+[Fire]: #user-content-fire
+```
+type Fire = (arguments: ...any) -> ()
+```
+
+A **Fire** function invokes all of the [Listeners][Listener] connected
+to the signal at the time Fire is called. Each given argument is passed to
+each listener. Each function listener is called in its own separate thread.
+
+The order in which listeners are invoked is **undefined**.
+
+After the signal is destroyed, calling this function throws an error.
+
 # Destroyer
 [Destroyer]: #user-content-destroyer
 ```
@@ -127,19 +163,50 @@ type Destroyer = () -> ()
 A **Destroyer** function destroys the signal by breaking all
 connections. After the signal is destroyed, calling the [Fire][Fire] or
 Destroyer functions will throw an error. The [Connector][Connector] function
-will continue to behave as though the signal is not destroyed.
+will do nothing but return a disconnector, which will also do nothing.
 
-# Fire
-[Fire]: #user-content-fire
+# Bindable
+[Bindable]: #user-content-bindable
 ```
-type Fire = (...any) -> ()
+type Bindable = {
+	Event   : Signal,
+	Fire    : (self: Bindable, ...any) -> (),
+	Destroy : (self: Bindable) -> (),
+}
 ```
 
-A **Fire** function invokes all of the [Listeners][Listener] connected
-to the signal at the time Fire is called. Each given argument is passed to
-each listener. Each function listener is called in its own separate thread.
+A **Bindable** implements the principle interface of
+[BindableEvent][BindableEvent].
 
-The order in which listeners are invoked is **undefined**.
+[BindableEvent]: https://developer.roblox.com/en-us/api-reference/class/BindableEvent
 
-After the signal is destroyed, calling the function throws an error.
+# Signal
+[Signal]: #user-content-signal
+```
+type Signal = {
+	Connect : (self: Signal, listener: Listener) -> (Connection),
+	Wait    : (self: Signal) -> (...any),
+}
+```
+
+A **Signal** implements the same interface as
+[RBXScriptSignal][RBXScriptSignal]. Not to be confused with the signal
+represented by an associated [Connector][Connector], [Fire][Fire] and
+[Destroyer][Destroyer].
+
+[RBXScriptSignal]: https://developer.roblox.com/en-us/api-reference/datatype/RBXScriptSignal
+
+# Connection
+[Connection]: #user-content-connection
+```
+type Connection = {
+	IsConnected : boolean,
+	Disconnect  : (self: Connection) -> (),
+}
+```
+
+A **Connection** implements the same interface as
+[RBXScriptConnection][RBXScriptConnection].
+
+[RBXScriptConnection]: https://developer.roblox.com/en-us/api-reference/datatype/RBXScriptConnection
 
--- modules/SignalFire/SignalFire.lua
+++ modules/SignalFire/SignalFire.lua
@@ -1,4 +1,4 @@
---v2.0.0
+--!strict
 
 --@sec: SignalFire
 --@ord: -1
@@ -9,12 +9,12 @@
 -- - Everything is a function.
 -- - Listeners may be threads as well as functions.
 -- - Listeners are always deferred.
--- - Listeners are unordered. Relying on this was always a bad idea.
--- - After a signal is fired, every listener with a connection *at the time of
---   firing* will be invoked.
+-- - Listeners are unordered.
+-- - After a signal is fired, only every listener with a connection *at the time
+--   of firing* will be invoked.
 --
 -- [observer]: https://en.wikipedia.org/wiki/Observer_pattern
-local SignalFire = {}
+local export = {}
 
 --@sec: Connector
 --@ord: 1
@@ -28,195 +28,362 @@ local SignalFire = {}
 --
 -- After the signal is destroyed, calling the function does nothing except
 -- return a disconnector, which also does nothing when called.
+export type Connector = (listener: Listener) -> Disconnector
 
 --@sec: Listener
 --@ord: 2
 --@def: type Listener = (...any) -> () | thread
 --@doc: A **Listener** receives the arguments passed to a [Fire][Fire] function.
+export type Listener = (...any) -> () | thread
 
 --@sec Disconnector
 --@ord: 3
 --@def: type Disconnector = () -> ()
 --@doc: A **Disconnector** breaks the connection of a [Listener][Listener] to a
 -- signal when called. Does nothing if the connection is already broken.
+export type Disconnector = () -> ()
 
 --@sec: Fire
 --@ord: 4
---@def: type Fire = (...any) -> ()
+--@def: type Fire = (arguments: ...any) -> ()
 --@doc: A **Fire** function invokes all of the [Listeners][Listener] connected
 -- to the signal at the time Fire is called. Each given argument is passed to
 -- each listener. Each function listener is called in its own separate thread.
 --
 -- The order in which listeners are invoked is **undefined**.
 --
--- After the signal is destroyed, calling the function throws an error.
+-- After the signal is destroyed, calling this function throws an error.
+export type Fire = (...any) -> ()
 
 --@sec: Destroyer
---@ord: 4
+--@ord: 5
 --@def: type Destroyer = () -> ()
 --@doc: A **Destroyer** function destroys the signal by breaking all
 -- connections. After the signal is destroyed, calling the [Fire][Fire] or
 -- Destroyer functions will throw an error. The [Connector][Connector] function
--- will continue to behave as though the signal is not destroyed.
+-- will do nothing but return a disconnector, which will also do nothing.
+export type Destroyer = () -> ()
 
 --@sec: SignalFire.new
 --@ord: 1
 --@def: function SignalFire.new(): (Connector, Fire, Destroyer)
 --@doc: The **new** constructor returns a signal, represented by associated
 -- [Connector][Connector], [Fire][Fire] and [Destroyer][Destroyer] functions.
-local function newSignal()
-	local listeners = {}
-	local function connect(listener)
+local function newSignal(): (Connector, Fire, Destroyer)
+	local listeners: {[Disconnector]: Listener} | nil = {}
+	local function connect(listener: Listener): Disconnector
 		assert(
 			type(listener) == "function" or
 			type(listener) == "thread",
-			"function or thread expected"
+			"listener must be a function or thread"
 		)
 		if listeners == nil then
 			return function()end
-		end
-		local function disconnect()
-			if listeners == nil then
-				return
+		else
+			local function disconnect()
+				if listeners == nil then
+					return
+				else
+					listeners[disconnect] = nil
+				end
 			end
-			listeners[disconnect] = nil
+			listeners[disconnect] = listener
+			return disconnect
 		end
-		listeners[disconnect] = listener
-		return disconnect
 	end
-	local function fire(...)
+	local function fire(...: any)
 		if listeners == nil then
 			error("signal is destroyed", 2)
-		end
-		for _, listener in pairs(listeners) do
-			task.defer(listener, ...)
+		else
+			for _, listener in pairs(listeners) do
+				task.defer(listener, ...)
+			end
 		end
 	end
 	local function destroy()
 		if listeners == nil then
 			error("signal is destroyed", 2)
+		else
+			for ref in pairs(listeners) do
+				listeners[ref] = nil
+			end
+			listeners = nil
 		end
-		for ref in pairs(listeners) do
-			listeners[ref] = nil
-		end
-		listeners = nil
 	end
 	return connect, fire, destroy
 end
-SignalFire.new = newSignal
+export.new = newSignal
 
-local function nopConnector(listener)
+local function nopConnector(listener: Listener): Disconnector
 	assert(
 		type(listener) == "function" or
 		type(listener) == "thread",
-		"function or thread expected"
+		"listener must be a function or thread"
 	)
 	return function()end
 end
 
 --@sec: SignalFire.all
---@ord: 2
---@def: function SignalFire.all(...: Connector): Connector
---@doc: The **all** constructor returns the [Connector][Connector] of a signal
--- that fires after all of the signals associated with the given connectors have
--- fired. The signal fires up to one time.
-function SignalFire.all(...)
+--@ord: 3
+--@def: function SignalFire.all(...: Connector): (Connector, Destroyer)
+--@doc: The **all** function returns the [Connector][Connector] and
+-- [Destroyer][Destroyer] of a signal that fires after all of the signals
+-- associated with the given connectors have fired. The signal will fire up to
+-- one time.
+function export.all(...: Connector): (Connector, Destroyer)
 	local arguments = table.pack(...)
 	local count = arguments.n
 	if count == 0 then
-		return nopConnector
+		return nopConnector, function()end
 	end
-	local connect, fire = newSignal()
+	local signalConnect, signalFire, signalDestroy: Destroyer? = newSignal()
+	local disconnectors: {[Disconnector]: true} = {}
 	for i = 1, arguments.n do
 		local connect = arguments[i]
-		assert(type(connect) == "function", "function expected")
-		local disconnect
-		local function finish()
-			if disconnect == nil then
-				return
-			end
-			disconnect()
-			disconnect = nil
-			count -= 1
-			if count <= 0 then
-				fire()
+		assert(type(connect) == "function", "connector must be a function")
+		local disconnect: Disconnector?
+		local function finish(...)
+			if disconnect then
+				local d = disconnect
+				disconnectors[disconnect] = nil
+				disconnect = nil
+				d()
+				count -= 1
+				if count <= 0 then
+					signalFire()
+				end
 			end
 		end
 		disconnect = connect(finish)
 		assert(type(disconnect) == "function", "disconnector must be a function")
+		disconnectors[disconnect] = true
 	end
-	return connect
+	local function destroy()
+		if signalDestroy then
+			for disconnect in pairs(disconnectors) do
+				disconnect()
+			end
+			table.clear(disconnectors)
+			local d = signalDestroy
+			signalDestroy = nil
+			task.defer(d)
+		else
+			error("signal is destroyed", 2)
+		end
+	end
+	return signalConnect, destroy
 end
 
 --@sec: SignalFire.any
---@ord: 2
---@def: function SignalFire.any(...: Connector): Connector
---@doc: The **any** constructor returns the [Connector][Connector] of a signal
--- that fires after any of the signals associated with the given connectors have
--- fired. The signal passes the arguments of the first signal that fired it. The
--- signal fires up to one time.
-function SignalFire.any(...)
-	local connections = table.pack(...)
-	if connections.n == 0 then
-		return nopConnector
+--@ord: 3
+--@def: function SignalFire.any(...: Connector): (Connector, Destroyer)
+--@doc: The **any** function returns the [Connector][Connector] and
+-- [Destroyer][Destroyer] of a signal that fires after any of the signals
+-- associated with the given connectors have fired. The signal passes the
+-- arguments of the first signal that fired it. The signal will fire until
+-- destroyed.
+function export.any(...: Connector): (Connector, Destroyer)
+	local arguments = table.pack(...)
+	if arguments.n == 0 then
+		return nopConnector, function()end
 	end
-	local connect, fire = newSignal()
-	local function finish(...)
-		if connections == nil then
-			return
-		end
-		for _, disconnect in ipairs(connections) do
-			disconnect()
-		end
-		connections = nil
-		fire(...)
-	end
-	for i = 1, connections.n do
-		local connect = connections[i]
-		assert(type(connect) == "function", "function expected")
-		local disconnect = connect(finish)
+	local signalConnect, signalFire, signalDestroy = newSignal()
+	local disconnectors: {Disconnector}? = table.create(arguments.n)
+	assert(disconnectors)
+	for i = 1, arguments.n do
+		local connect = arguments[i]
+		assert(type(connect) == "function", "connector must be a function")
+		local disconnect = connect(signalFire)
 		assert(type(disconnect) == "function", "disconnector must be a function")
-		connections[i] = disconnect
+		disconnectors[i] = disconnect
 	end
-	return connect
+	local function destroy()
+		if disconnectors == nil then
+			error("signal is destroyed", 2)
+		else
+			local d = disconnectors
+			disconnectors = nil
+			for _, disconnect in ipairs(d) do
+				disconnect()
+			end
+			table.clear(d)
+			-- Must be deferred, because calls to signalFire are deferred.
+			task.defer(signalDestroy)
+		end
+	end
+	return signalConnect, destroy
 end
 
 --@sec: SignalFire.wait
---@ord: 2
+--@ord: 3
 --@def: function SignalFire.wait(connect: Connector): (() -> (...any))
---@doc: The **wait** constructor returns a function that, when called, yields
--- the running thread. The thread is resumed after the signal associated with
+--@doc: The **wait** function returns a function that, when called, yields the
+-- running thread. The thread is resumed after the signal associated with
 -- *connect* fires, returning the arguments passed through the signal.
-function SignalFire.wait(connect)
-	assert(type(connect) == "function", "function expected")
+local function waitSignal(connect: Connector): (() -> (...any))
+	assert(type(connect) == "function", "connector must be a function")
 	return function()
 		local thread = coroutine.running()
-		local disconnect
+		local disconnect: Disconnector?
 		disconnect = connect(function(...)
-			disconnect()
-			task.defer(thread, ...)
+			if disconnect then
+				local d = disconnect
+				disconnect = nil
+				d()
+				task.defer(thread, ...)
+			end
 		end)
 		assert(type(disconnect) == "function", "disconnector must be a function")
 		return coroutine.yield()
 	end
 end
+export.wait = waitSignal
+
+--@sec: SignalFire.limit
+--@ord: 3
+--@def: function SignalFire.limit(connect: Connector, limit: number?): Connector
+--@doc: The **limit** function wraps *connect*, returning a
+-- [Connector][Connector] that will cause its connected [Listeners][Listener] to
+-- be fired only up to *limit* times. The limit defaults to 1.
+function export.limit(connect: Connector, limit: number?): Connector
+	assert(type(connect) == "function", "connector must be a function")
+	assert(type(limit) == "number" or limit == nil, "limit must be a number or nil")
+	local n = limit or 1
+	n = math.floor(n)
+	assert(n >= 0, "limit must be a positive integer")
+	if n == 0 then
+		return nopConnector
+	end
+	return function(listener: Listener)
+		local disconnect: Disconnector?
+		disconnect = connect(function(...)
+			if disconnect then
+				n -= 1
+				if n <= 0 then
+					local d = disconnect
+					disconnect = nil
+					d()
+				end
+				task.spawn(listener, ...)
+			end
+		end)
+		return assert(disconnect, "disconnector must be a function")
+	end
+end
 
 --@sec: SignalFire.wrap
---@ord: 2
+--@ord: 3
 --@def: function SignalFire.wrap(signal: RBXScriptSignal): Connector
---@doc: The **wrap** constructor returns a [Connector][Connector] that wraps
--- *signal*. [Listeners][Listener] passed to the connector must be passable to
--- [RBXScriptSignal.Connect][Connect].
---
--- [Connect]: https://developer.roblox.com/en-us/api-reference/datatype/RBXScriptSignal#functions
-function SignalFire.wrap(signal)
+--@doc: The **wrap** function returns a [Connector][Connector] that wraps
+-- *signal*. If the connector is passed a thread as a [Listener][Listener], it
+-- is connected via a function that calls task.defer with the thread and the
+-- received arguments.
+function export.wrap(signal: RBXScriptSignal): Connector
 	assert(typeof(signal) == "RBXScriptSignal", "RBXScriptSignal expected")
-	return function(listener)
-		local connection = signal:Connect(listener)
-		return function()
-			connection:Disconnect()
+	return function(listener: Listener)
+		if type(listener) == "function" then
+			local connection = signal:Connect(listener)
+			return function()
+				connection:Disconnect()
+			end
+		elseif type(listener) == "thread" then
+			local connection = signal:Connect(function(...)
+				task.defer(listener, ...)
+			end)
+			return function()
+				connection:Disconnect()
+			end
+		else
+			error("listener must be a function or thread", 2)
 		end
 	end
 end
 
-return SignalFire
+--@sec: Bindable
+--@ord: 10
+--@def: type Bindable = {
+-- 	Event   : Signal,
+-- 	Fire    : (self: Bindable, ...any) -> (),
+-- 	Destroy : (self: Bindable) -> (),
+-- }
+--@doc: A **Bindable** implements the principle interface of
+-- [BindableEvent][BindableEvent].
+--
+-- [BindableEvent]: https://developer.roblox.com/en-us/api-reference/class/BindableEvent
+export type Bindable = {
+	Event: Signal,
+	Fire: (self: Bindable, ...any) -> (),
+	Destroy: (self: Bindable) -> (),
+}
+
+--@sec: Signal
+--@ord: 11
+--@def: type Signal = {
+-- 	Connect : (self: Signal, listener: Listener) -> (Connection),
+-- 	Wait    : (self: Signal) -> (...any),
+-- }
+--@doc: A **Signal** implements the same interface as
+-- [RBXScriptSignal][RBXScriptSignal]. Not to be confused with the signal
+-- represented by an associated [Connector][Connector], [Fire][Fire] and
+-- [Destroyer][Destroyer].
+--
+-- [RBXScriptSignal]: https://developer.roblox.com/en-us/api-reference/datatype/RBXScriptSignal
+export type Signal = {
+	Connect: (self: Signal, listener: Listener) -> (Connection),
+	Wait: (self: Signal) -> (...any),
+}
+
+--@sec: Connection
+--@ord: 12
+--@def: type Connection = {
+-- 	IsConnected : boolean,
+-- 	Disconnect  : (self: Connection) -> (),
+-- }
+--@doc: A **Connection** implements the same interface as
+-- [RBXScriptConnection][RBXScriptConnection].
+--
+-- [RBXScriptConnection]: https://developer.roblox.com/en-us/api-reference/datatype/RBXScriptConnection
+export type Connection = {
+	IsConnected: boolean,
+	Disconnect: (self: Connection) -> (),
+}
+
+--@sec: SignalFire.bindable
+--@ord: 2
+--@doc: The **bindable** constructor returns a new [Bindable][Bindable],
+-- implemented using the functions from [SignalFire.new][SignalFire.new].
+function export.bindable(): (Bindable)
+	local signalConnect, signalFire, signalDestroy = newSignal()
+	local signalWait = waitSignal(signalConnect)
+
+	local signal = {
+		Connect = function(self: Signal, listener: Listener): Connection
+			local disconnect = signalConnect(listener)
+			local connection = {
+				IsConnected = true,
+				Disconnect = function(self: Connection)
+					self.IsConnected = false
+					disconnect()
+				end,
+			}
+			return connection
+		end,
+		Wait = function(self: Signal): (...any)
+			return signalWait()
+		end,
+	}
+
+	local bindable = {
+		Event = signal,
+		Fire = function(self: Bindable, ...: any)
+			signalFire(...)
+		end,
+		Destroy = function(self: Bindable)
+			signalDestroy()
+		end,
+	}
+
+	return bindable
+end
+
+return export
